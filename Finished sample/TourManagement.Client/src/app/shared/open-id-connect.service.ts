1d6831c7f74d57da97b6e53af3f77dc089a7f26b
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-07-14 18:22:12 -0400

    Comments

--- Finished sample/TourManagement.Client/src/app/shared/open-id-connect.service.ts
+++ Finished sample/TourManagement.Client/src/app/shared/open-id-connect.service.ts
@@ -57,6 +57,10 @@ export class OpenIdConnectService {
     });
   }
 
+  // ************ This method will parse the url for the hash fragment and take some of that content and create
+  //              a user object that it will stuff in Session Storage
+  //              The user is now signed in with the IdentityServer and even if I delete the SessionStorage and refresh the
+  //              Angular application, the user object gets recreated behind the scenes ...
   handleSilentCallback() {
     this.userManager.signinSilentCallback().then(function (user) {
       this.currentUser = user
@@ -66,6 +70,7 @@ export class OpenIdConnectService {
     });
   }
 
+  // ************* So, we can sign the user out with the IdenityServer4
   triggerSignOut() {
     this.userManager.signoutRedirect().then(function (resp) {
       if (!environment.production) {
