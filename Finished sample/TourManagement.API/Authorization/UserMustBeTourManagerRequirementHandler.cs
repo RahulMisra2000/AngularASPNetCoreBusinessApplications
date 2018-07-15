41ab3e7895930f5f133a22cce8b6fbe8021f4594
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-07-15 00:08:45 -0400

    Comments

--- Finished sample/TourManagement.API/Authorization/UserMustBeTourManagerRequirementHandler.cs
+++ Finished sample/TourManagement.API/Authorization/UserMustBeTourManagerRequirementHandler.cs
@@ -8,53 +8,43 @@ using TourManagement.API.Services;
 
 namespace TourManagement.API.Authorization
 {
-    public class UserMustBeTourManagerRequirementHandler
-        : AuthorizationHandler<UserMustBeTourManagerRequirement>
+    public class UserMustBeTourManagerRequirementHandler : AuthorizationHandler<UserMustBeTourManagerRequirement>
     {
         private readonly ITourManagementRepository _tourManagementRepository;
         private readonly IUserInfoService _userInfoService;
 
-        public UserMustBeTourManagerRequirementHandler(
-            ITourManagementRepository tourManagementRepository,
-            IUserInfoService userInfoService)
+        public UserMustBeTourManagerRequirementHandler(ITourManagementRepository tmr, IUserInfoService uis)
         {
-            _tourManagementRepository = tourManagementRepository;
-            _userInfoService = userInfoService;
+            _tourManagementRepository = tmr;
+            _userInfoService = uis;
         }
 
-        protected override Task HandleRequirementAsync(
-            AuthorizationHandlerContext context, UserMustBeTourManagerRequirement requirement)
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UserMustBeTourManagerRequirement requirement)
         {
-            if (_userInfoService.Role == requirement.Role)
-            {
+            if (_userInfoService.Role == requirement.Role) {
                 context.Succeed(requirement);
                 return Task.FromResult(0);
             }
 
             var filterContext = context.Resource as AuthorizationFilterContext;
-            if (filterContext == null)
-            {
+            if (filterContext == null) {
                 context.Fail();
                 return Task.FromResult(0);
             }
 
             var tourId = filterContext.RouteData.Values["tourId"].ToString();
 
-            if (!Guid.TryParse(tourId, out Guid tourIdAsGuid))
-            {
+            if (!Guid.TryParse(tourId, out Guid tourIdAsGuid)) {
                 context.Fail();
                 return Task.FromResult(0);
             }
 
-            if (!Guid.TryParse(_userInfoService.UserId, out Guid userIdAsGuid))
-            {
+            if (!Guid.TryParse(_userInfoService.UserId, out Guid userIdAsGuid)) {
                 context.Fail();
                 return Task.FromResult(0);
             }
 
-
-            if (!_tourManagementRepository.IsTourManager(tourIdAsGuid, userIdAsGuid).Result)
-            {
+            if (!_tourManagementRepository.IsTourManager(tourIdAsGuid, userIdAsGuid).Result) {
                 context.Fail();
                 return Task.FromResult(0);
             }
